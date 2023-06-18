8a9db134b6365bfef0fa71374a4ed69aa8bc93d2
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-06-18 16:05:09 +0300

    done

--- README.md
+++ README.md
@@ -8,20 +8,22 @@ https://drive.google.com/drive/folders/1vxwNf_qo1MMYqcZk1pPFlbw_KZuDaxUO
 
 ### Дії, які підтримує додаток
 
-Нижче описані команди:
+<br/>
 
-#### Отримати в терміналі весь список контактів
+Нижче описані команди👇🏻
+
+#### Отримати в терміналі весь список контактів:
 
 node index.js --action="list" // node index --action list
 
-#### Отримати контакт по id - будe виведено в термінал обʼєкт або null, якщо контакту з таким id не існує
+#### Отримати контакт по id - будe виведено в термінал обʼєкт або null, якщо контакту з таким id не існує:
 
-node index.js --action="get" --id <your id> // node index --action get --id <your id>
+node index.js --action="get" --id your-id // node index --action get --id your-id
 
-#### Додати контакт - будe виведено в термінал обʼєкт
+#### Додати контакт - будe виведено в термінал обʼєкт:
 
-node index.js --action="add" --name <your name> --email <your email> --phone <your phone> // node index --action add --name <your name> --email <your email> --phone <your phone>
+node index.js --action="add" --name your-name --email your-email --phone your-phone // node index --action add --name your-name --email your-email --phone your-phone
 
-#### Видалити контакт - будe виведено в термінал обʼєкт видаленого контакту або null, якщо контакту з таким id не існує
+#### Видалити контакт - будe виведено в термінал обʼєкт видаленого контакту або null, якщо контакту з таким id не існує:
 
-node index.js --action="remove" --id <your id> // node index --action remove <your id>
+node index.js --action="remove" --id your-id // node index --action remove your-id
--- contacts.js
+++ contacts.js
@@ -5,41 +5,57 @@ const { nanoid } = require("nanoid");
 const contactsPath = path.join(__dirname, "db/contacts.json");
 
 async function listContacts() {
-  const data = await fs.readFile(contactsPath);
-  return JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getContactById(contactId) {
-  const strID = String(contactId);
-  const contacts = await listContacts();
-  const contact = contacts.find((item) => item.id === strID);
-  return contact || null;
+  try {
+    const strID = String(contactId);
+    const contacts = await listContacts();
+    const contact = contacts.find((item) => item.id === strID);
+    return contact || null;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function removeContact(contactId) {
-  const strID = String(contactId);
-  const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === strID);
-  if (index === -1) {
-    return null;
+  try {
+    const strID = String(contactId);
+    const contacts = await listContacts();
+    const index = contacts.findIndex((item) => item.id === strID);
+    if (index === -1) {
+      return null;
+    }
+    const [contact] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return contact;
+  } catch (error) {
+    console.log(error);
   }
-  const [contact] = contacts.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return contact;
 }
 
 async function addContact(name, email, phone) {
-  const contacts = await listContacts();
-  const strPhone = String(phone);
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone: strPhone,
-  };
-  contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return newContact;
+  try {
+    const contacts = await listContacts();
+    const strPhone = String(phone);
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone: strPhone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {
